c1e6d82c5a1208e9010f1d9bdddf3c3c8828a1be
Максим <maxibredikhin@gmail.com>
2024-02-07 19:54:04 +0400

    Создали задачу два, где создали массив и заполнили его целыми числами от 1 до n

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10; //n это переменная
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length) //arr.Length вместо переменной n
+{
+    arr[i] = i + 1;
+    //System.Console.WriteLine(arr[i]);
+    //System.Console.WriteLine(' ');
+    Console.Write($"{arr[i]} "); //упрощение кода
+    i = i + 1;
+}
